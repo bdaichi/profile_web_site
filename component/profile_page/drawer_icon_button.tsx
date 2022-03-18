07c54f9fd2f99a0dfbdde3d367614b52a0ffc7bc
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-18 10:53:55 +0900

    コンポーネントを分けた

---
+++ component/profile_page/drawer_icon_button.tsx
@@ -0,0 +1,26 @@
+import { IconButton } from "@mui/material";
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+type Props = {
+    isOpenDrawer: boolean
+    openDrawer: () => void
+    closeDrawer: () => void
+}
+
+export default function DrawerIconButton(props: Props) {
+
+    return (
+        <>
+            {!props.isOpenDrawer ?
+                <IconButton onClick={props.openDrawer} className='absolute right-9'>
+                    <ExpandMoreIcon />
+                </IconButton>
+                :
+                <IconButton onClick={props.closeDrawer} className='absolute right-9'>
+                    <ExpandLessIcon />
+                </IconButton>
+            }
+        </>
+    )
+}
---
+++ component/profile_page/pg_career_field.tsx
@@ -0,0 +1,23 @@
+export default function PGCareerField() {
+
+    return (
+        <div className='flex flex-col'>
+            <p className='flex justify-center text-lg py-4'>プログラミングを始めたきっかけ</p>
+            <p className='flex justify-center text-base tracking-widest whitespace-pre-wrap mx-4 p-7 border border-gray-400 rounded-md'>
+                中学生の時にパソコンができる人に憧れを抱いていて、かっこいいなと思っていました。
+                家の近くの高校の商業科に学力特待生として合格できたのでそこに入学しました。
+                高校でWord、Excelを学びプログラミングにも興味が出てきたのですが、プログラミングの授業はなく
+                教えられる先生もいなかったので、独学でプログラミングの学習をはじめました。
+            </p>
+            <p className='flex justify-center text-lg mt-8 py-4'>プログラミングに対する気持ち</p>
+            <p className='flex justify-center text-base tracking-widest whitespace-pre-wrap mx-4 p-7 border border-gray-400 rounded-md'>
+                プログラミングを始めたばかりの頃はわからないことだらけで難しいという印象でした。
+                途中で何度も挫折したんですが、どうしてもプログラミングができるようになって、
+                プログラミングを好きになりたい、エンジニアとして将来働きたい、
+                という思いが強かったので、立ち上がることができました。
+                今ではプログラミングが好きだと思えることが多くなってきました。
+                そして、この先プログラミングのことを好きでいたいという気持ちはずっと持ち続けたいです。
+            </p>
+        </div>
+    )
+}
