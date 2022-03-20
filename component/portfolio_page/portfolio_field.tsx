cde438f94d160047796db70e6f7d4a6d2b0ad018
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-20 18:21:51 +0900

    リンクのcssを改良した

--- component/portfolio_page/portfolio_field.tsx
+++ component/portfolio_page/portfolio_field.tsx
@@ -35,11 +35,11 @@ export default function PortfolioField(props: Props) {
                     <p className='flex justify-center text-4xl'>{props.portfolioName}</p>
                     <img className='m-8' src={props.imagePath} />
                 </div>
-                <div className='flex flex-col justify-center'>{props.portfolioURL &&
-                    <>
-                        <p className='flex justify-center  md:absolute md:inset-x-0 md:top-52' style={{ color: props.isDarkMode ? 'white' : 'black' }}>こちらから開けます！ぜひ！どうぞ！！</p>
-                        <a className='flex justify-center border-b border-blue-400 mx-12 md:absolute md:inset-x-0 md:mx-72 md:top-60' style={{ color: '#00a9f4', }} href={props.portfolioURL}>{props.portfolioURL}</a>
-                    </>
+                <div className='flex flex-col justify-center items-center'>{props.portfolioURL &&
+                    <div className="flex flex-col justify-center items-center">
+                        <p className='flex justify-center  md:absolute md:top-52' style={{ color: props.isDarkMode ? 'white' : 'black' }}>こちらから開けます！ぜひ！どうぞ！！</p>
+                        <a className='flex justify-center border-b border-blue-400 md:absolute md:top-60' style={{ color: '#00a9f4', }} href={props.portfolioURL}>{props.portfolioURL}</a>
+                    </div>
                 }</div>
                 <p className='flex flex-row justify-center items-center whitespace-pre-wrap text-base rounded-md border border-gray-400 p-12 mx-8 my-8 md:w-96' style={{ color: props.isDarkMode ? 'white' : 'black', borderColor: props.isDarkMode ? '#01a9f4' : 'black', }}>{props.summary}</p>
             </div>
