30876a00f8407429ac5cab06ddc4790cd489b2e5
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-20 15:56:06 +0900

    ポートフォリオの開発について補足した

--- pages/portfolio_page.tsx
+++ pages/portfolio_page.tsx
@@ -24,7 +24,7 @@ export default function PortfolioPage() {
     const query = router.query;
     const portfolioName = query.portfolioName as string;
 
-    const storeListSummary: string = '買い物用のwebアプリです。僕が初めて公開したアプリになります。とても愛着が湧いていて、スーパーへ買い物によく行くので愛用してます。僕はエラーが出て解決するのに時間がかかったら兄に教えてもらっています。このWebアプリは自作アプリ第３号で、エラーが出てもほぼ自分で解決できるようになりました。成長を感じられて嬉しかったです。これは初の人にお見せできるwebアプリです'
+    const storeListSummary: string = '買い物用のwebアプリです。僕が初めて公開したアプリになります。とても愛着が湧いていて、スーパーへ買い物によく行くので愛用してます。僕はエラーが出て解決するのに時間がかかったら兄に教えてもらっています。このWebアプリは自作アプリ第３号で、エラーが出てもほぼ自分で解決できるようになりました。成長を感じられて嬉しかったです。これは初の人にお見せできるwebアプリです。React、TypeScript、Firebaseを使いました。他の２つのアプリも同じです。'
 
     const visitorForecastAppSummary: string = '過去の来客数から、その日の来客数を予測するというwebアプリです。予測方法が単純、その日の予想人数しか出せないなど実用化できるレベルにはないので、公開してません。'
 
