2742a88e1e6c3f3dff96068f8ccf95477f4c5bf4
別府大地 <beppudaichi@beppudaichis-MacBook-Air.local>
2022-03-21 13:56:29 +0900

    プログラミングの感想を書いた

--- component/profile_page/pg_career_field.tsx
+++ component/profile_page/pg_career_field.tsx
@@ -14,6 +14,23 @@ export default function PGCareerField(props: Props) {
                 高校でWord、Excelを学びプログラミングにも興味が出てきたのですが、プログラミングの授業はなく
                 教えられる先生もいなかったので、独学でプログラミングの学習をはじめました。
             </p>
+            <p className='flex justify-center text-lg mt-8 py-4' style={{ color: props.isDarkMode ? '#01a9f4' : 'black' }}>プログラミングをやってみて</p>
+            <p className='flex justify-center text-base tracking-widest whitespace-pre-wrap mx-4 p-7 border border-gray-400 rounded-md' style={{ color: props.isDarkMode ? 'white' : 'black', borderColor: props.isDarkMode ? '#01a9f4' : 'black', }}>
+                「大変だったな〜」の一言につきます(笑)。
+                専門学生になってアプリ開発を始めたのですが、
+                環境構築、APIなど、当然わからないことだらけで学習の毎日でした。
+                その学習の中でも、僕がプログラミングでかなり大事だと思ったことがコードの書き方です。
+                最初は機能を完成させることに集中していて、変数名をupdateなどの単語１つだけで、
+                なにを更新するのか後から見てわからなかったり、１つのクラス、関数にたくさんコードを書いていて
+                どこでエラーが出ているかわかりにくかったりしたので、コードを綺麗に読みやすく書くために、
+                ドメイン駆動、リーダブルコードなどの参考書を読みながらコードを書きました。
+                変数名は多少長くなってもいいから、
+                読む人にとってわかりやすいようにと考えてつけるようにしました。
+                また、コードを書く際には、値オブジェクトには何が必要なのか決め、その値オブジェクトをもとにドメインサービスを書き、
+                関数、コンポーネントなどは組み合わせ方、使い方によってコードをどこまで分けるのか決めていました。
+                まだまだ理解が浅く、たまに変数名がわかりにくかったり見にくいコードがあったりするので、
+                常に意識しながらコードを書く癖をつけたいです。
+            </p>
             <p className='flex justify-center text-lg mt-8 py-4' style={{ color: props.isDarkMode ? '#01a9f4' : 'black' }}>プログラミングに対する気持ち</p>
             <p className='flex justify-center text-base tracking-widest whitespace-pre-wrap mx-4 p-7 border border-gray-400 rounded-md' style={{ color: props.isDarkMode ? 'white' : 'black', borderColor: props.isDarkMode ? '#01a9f4' : 'black', }}>
                 プログラミングを始めたばかりの頃はわからないことだらけで難しいという印象でした。
